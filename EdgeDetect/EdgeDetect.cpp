eb09801e61a2518b2b3c8f42c2b9dc710a7113a6
YitongFeng <imfengyitong@hotmail.com>
2016-03-24 14:19:27 +0800

    Success! Note: Remember to transfer to gray image before transferring…

--- EdgeDetect/EdgeDetect.cpp
+++ EdgeDetect/EdgeDetect.cpp
@@ -8,31 +8,49 @@ using namespace std;
 //#define debug
 
 int main(){
-	float thresh = 1 / 16;
+	float thresh = 0.18;
 	Mat src = imread("b100001.jpg");
-	Mat img; 
 	if (!src.data){
 		cout << "Load image failed!" << endl;
 		return -1;
 	}
-	threshold(src, img, 127, 255, THRESH_BINARY);
+
+	resize(src, src, Size(src.cols / 4, src.rows / 4));
+	Mat img; 
+	cvtColor(src, img, CV_BGR2GRAY);
+	threshold(img, img, 127, 255, THRESH_BINARY);
+	erode(img, img, Mat(3, 3, CV_8U));
 	//medianBlur(img, img, 3);
 	//blur(src, img, Size(3,3));
-	resize(img, img, Size(img.cols / 4, img.rows / 4));
+	//resize(img, img, Size(img.cols / 4, img.rows / 4));
+	
 	int nr = img.rows;
 	int nc = img.cols;
 	
 	for (int j = 5; j < nc / 2; j++){
 		int count = 0;
+		for (int i = 5; i < nr; i++){
+			if ( (int)(img.at<uchar>(i, j)) < 10)
+				count++;
+		}
+		if(count > (thresh * nr)){
+			line(src, Point(j - 1, 0), Point(j - 1, nr), Scalar(0, 0, 255));
+			break;
+		}
+	}
+
+	for (int j = nc - 5; j > nc / 2; j--){
+		int count = 0;
 		for (int i = 5; i < nr / 2; i++){
 			if ( (int)(img.at<uchar>(i, j)) < 10)
 				count++;
 		}
-		if(count > thresh * nr){
-			line(img, Point(j - 1, 0), Point(j - 1, nr), Scalar(0, 0, 255));
+		if (count > thresh * nr){
+			line(src, Point(j + 1, 0), Point(j + 1, nr), Scalar(0, 0, 255));
 			break;
 		}
 	}
+
 #ifdef debug1
 			cout << "Point value: " << Point(j - 1, i) << " " << (int)data[j] << endl;
 #endif
@@ -44,7 +62,8 @@ int main(){
 #endif
 		
 	namedWindow("picture");
-	imshow("picture", img);
+	imshow("picture", src);
+	imshow("img", img);
 	waitKey();
 	return 0;
 }
